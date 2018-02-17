84290c4422270be975f687705049ffbacc2bfff5
Daria Marczak <marczak.ria@gmail.com>
2018-02-17 14:23:45 +0100

    Added a return

--- server/controllers/lane.controller.js
+++ server/controllers/lane.controller.js
@@ -3,14 +3,14 @@ import uuid from 'uuid';
 
 export function addLane(req, res) {
   if (!req.body.name) {
-    res.status(403).end();
+    return res.status(403).end();
   }
 
   const newLane = new Lane(req.body);
 
   newLane.notes = [];
+
   newLane.id = uuid();
-  
   newLane.save((err, saved) => {
     if (err) {
       res.status(500).send(err);
